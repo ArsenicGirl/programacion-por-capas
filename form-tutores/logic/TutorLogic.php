6baa9f208d520c01c44c1c5c62478786ad1307b2
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2024-08-15 20:49:23 -0500

    actualizacion de DTO

--- form-tutores/logic/TutorLogic.php
+++ form-tutores/logic/TutorLogic.php
@@ -1,5 +1,46 @@
 <?php
+require_once('TutorDAO.php');
+require_once('TutorDTO.php');
 class TutorLogic{
+    private $tutorDAO;
+
+    public function __contruct(){
+        $this->tutorDAO = new TutorDAO();
+
+    }
+
+    //Traer el metodo de leer todos los datos
+    public function getReadTutor(){
+        return $this->tutorDAO->readTutor();
+    }
+
+     //Traer el metodo de leer todos los datos pero por Id
+    public function getReadById($id){
+        return $this->tutorDAO->readById($id);
+    }
+
+    //Traer el metodo de crear tutor
+    public function createTutor($name, $phone, $is_director){
+        // Crear el objeto DTO
+        $tutorDTO = new TutorDTO(null, $name, $phone, (bool)$is_director);
+
+        //lógica para crear un tutor usando DAO
+        $this->tutorDAO->createTutor($tutorDTO);
+    }
+
+    //Traer el metodo de actualizar al tutor
+    public function updateTutor($id, $name, $phone, $is_director){
+        //crear el objeto DTO
+        $tutorDTO = new TutorDTO($id, $name, $phone, (bool)$is_director);
+
+        $this->tutorDAO->updateTutor($tutorDTO);
+    }
+
+    //Traer el metodo para eliminar un tutor
+    public function deleteTutor($id){
+        $this->tutorDAO->deleteTutor($id);
+    }
+
 
 }
 ?>
