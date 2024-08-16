8d597223e78827557d839f48ce1e39a53b77c3e0
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2024-08-16 00:28:54 -0500

    interfaz

--- form-tutores/presentacion/TutorForm.php
+++ form-tutores/presentacion/TutorForm.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="../logica/AccionesForm.php"></script>
+    <link rel="stylesheet" href="TutorForm.css">
     <title>Document</title>
 </head>
 <body>
@@ -13,20 +14,22 @@
     <!-- Formulario para crear y actualizar -->
      <div class="contenedor-formularios">
         <div class="contenedor-post">
-            <form action="TutorForm.php" method="post">
+            <form action="TutorForm.php" method="post"><br>
                 <input type="hidden" name="id" id="id" value=""> <!-- Este campo se debe rellenar solo cuando se actualice -->
 
-                <label for="name">Nombre del tutor</label>
+                <label for="name">Nombre del tutor</label><br>
                 <input type="text" name="name" id="name" required><br>
 
-                <label for="phone">Número de telefono</label>
+                <label for="phone">Número de telefono</label><br>
                 <input type="tel" name="phone" id="phone" required><br>
 
                 <label for="is_director">¿Posee el cargo de director?</label>
                 <input type="checkbox" name="is_director" id="is_director" value="1"><br>
 
-                <button type="submit" name="action" value="create">Crear tutor</button><br>
-                <button type="submit" name="action" value="update">Actualizar tutor</button>
+                <div class="button-group">
+                    <button type="submit" name="action" value="create">Crear tutor</button><br>
+                    <button type="submit" name="action" value="update">Actualizar tutor</button>
+                </div>
             </form>
         </div>
 
@@ -35,7 +38,7 @@
                 <label for="delete_id">ID del tutor a Eliminar:</label>
                 <input type="number" name="delete_id" id="delete_id" required>
 
-                <button type="submit" name="action" value="delete">Eliminar</button>
+                <button type="submit" name="action" value="delete" class="button-delete">Eliminar</button>
             </form>
     
         </div>
@@ -52,6 +55,7 @@
                     <th>Nombre</th>
                     <th>Teléfono</th>
                     <th>¿Es director?</th>
+                    <th>Editar</th>
                 </tr>
             </thead>
             <tbody>
